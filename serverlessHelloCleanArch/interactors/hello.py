cf16a21b0bc11adf29d5b747ca38dda1c52f9e01
himanshu22500 <himanshu22500@gmail.com>
2024-05-24 17:50:49 +0530

    add folder structure

---
+++ serverlessHelloCleanArch/interactors/hello.py
@@ -0,0 +1,3 @@
+class HelloInteractor:
+    pass
+
---
+++ serverlessHelloCleanArch/models/Message.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from uuid import uuid4
+
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Message(Base):
+    __tablename__ = 'users'
+
+    id = Column(String(255), primary_key=True, default=lambda: str(uuid4()))
+    created_at = Column(DateTime, default=datetime.now)
+    text = Column(String(255))
+
+    @classmethod
+    def create_message(cls, text):
+        pass
